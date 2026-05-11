f4ec96a7578c555ec118af94ee6fc6ac88767743
chloelulu369-art <chloe.lulu369@gmail.com>
2026-05-11 17:08:41 -0300

    Create index.js
    esta la base no testeasa
    repasar como usar colores en js

---
+++ index.js
@@ -0,0 +1,24 @@
+script src="./gamecontroller.min.js"></script>
+function conectado(){
+    gameControl.on('connect', gamepad => {
+  console.log('A new gamepad was connected!');
+//cambiar de color verde, preguntar si esta listo para la carrera, cuenta regresiva
+
+});
+
+!function conectado(){
+    gameControl.on('disconnect', gamepad);
+    
+
+}
+function rotacion(){
+ if(gameControl.set('up0', 0.5)){
+      console.log('arriba precionado');
+//cambiar imagen de automovilista
+ }
+}
+function mario(){
+    //asignar tamaño de la caja de imagen, centrado abajo
+
+    //cambiar imagen dependiendo de el valor de la variable
+}

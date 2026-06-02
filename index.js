9390ac1fc2539dc0b82fd456cc3580fa4d19c012
chloelulu369-art <chloe.lulu369@gmail.com>
2026-06-02 15:57:39 -0300

    Update index.js
    funciones creadas, arreglo a medias de el tema de colores

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ script src="./gamecontroller.min.js"></script>
 function conectado(){
     gameControl.on('connect', gamepad => {
   console.log('A new gamepad was connected!');
-//cambiar de color verde, preguntar si esta listo para la carrera, cuenta regresiva
+nos códigos de color verde:
+changeBackgroundColor("#008f39");
+delay(500)
+changeBackgroundColor("{3E9B6367-8F44-4700-8C8F-C47283765B2F}.png");
 
 });
 
@@ -11,14 +14,26 @@ function conectado(){
     changeBackgroundColor("#ff0000");
 
 }
-function rotacion(){
+function up(){
  if(gameControl.set('up0', 0.5)){
       console.log('arriba precionado');
 //cambiar imagen de automovilista
  }
 }
-function mario(){
-    //asignar tamaño de la caja de imagen, centrado abajo
-
-    //cambiar imagen dependiendo de el valor de la variable
-}
+function left(){
+ if(gameControl.set('left0', 0.5)){
+      console.log('izquierda precionado');
+//cambiar imagen de automovilista
+ }
+    }
+function right(){
+ if(gameControl.set('right0', 0.5)){
+      console.log('derecha precionado');
+//cambiar imagen de automovilista
+ }
+    }
+function down(){
+ if(gameControl.set('down0', 0.5)){
+      console.log('down precionado');
+//cambiar imagen de automovilista
+ }

1fcf39082a75e81639dde517e09888cb3056ecc1
chloelulu369-art <chloe.lulu369@gmail.com>
2026-06-09 17:24:18 -0300

    papas con mayonesa
    se mejoro el concepto de captura de el auto;
    falta declarar los estilos y entender como cambian del css a raiz de cambiar la class.
    falta acomodar el auto en posision correcta

---
+++ mandowiiweb-main/index.js
@@ -0,0 +1,39 @@
+script src="./gamecontroller.min.js"></script>
+function conectado(){
+    gameControl.on('connect', gamepad => {
+  console.log('A new gamepad was connected!');
+nos códigos de color verde:
+changeBackgroundColor("#008f39");
+delay(500)
+changeBackgroundColor("{3E9B6367-8F44-4700-8C8F-C47283765B2F}.png");
+
+});
+
+!function conectado(){
+    gameControl.on('disconnect', gamepad);
+    changeBackgroundColor("#ff0000");
+
+}
+function up(){
+ if(gameControl.set('up0', 0.5)){
+      console.log('arriba precionado');
+//cambiar imagen de automovilista
+ }
+}
+function left(){
+ if(gameControl.set('left0', 0.5)){
+      console.log('izquierda precionado');
+//cambiar imagen de automovilista
+ }
+    }
+function right(){
+ if(gameControl.set('right0', 0.5)){
+      console.log('derecha precionado');
+//cambiar imagen de automovilista
+ }
+    }
+function down(){
+ if(gameControl.set('down0', 0.5)){
+      console.log('down precionado');
+//cambiar imagen de automovilista
+ }
